ca9b8f99435b0973ae90d5d121b706b7de4b7afa
randomslap <kentokazaki@gmail.com>
2022-08-29 18:26:33 -0700

    removed unused variables

--- App.tsx
+++ App.tsx
@@ -1,18 +1,10 @@
-import React, { type PropsWithChildren, useEffect } from "react"
-import { Platform, StatusBar, StyleSheet, useColorScheme } from "react-native"
+import React, { useEffect } from "react"
+import { Platform, StatusBar } from "react-native"
 import SplashScreen from "react-native-splash-screen"
-
-import { Colors } from "react-native/Libraries/NewAppScreen"
 
 import Navigation from "./src/navigation"
 
 const App = () => {
-	const isDarkMode = useColorScheme() === "dark"
-
-	const backgroundStyle = {
-		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-	}
-
 	useEffect(() => {
 		if (Platform.OS === "android") {
 			StatusBar.setBackgroundColor("rgba(0,0,0,0)")
